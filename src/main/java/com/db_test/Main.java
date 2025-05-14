c5d616075f40af962507cac63fca64703ac9788e
SoPyayTun <xray191219@gmail.com>
2025-05-14 11:44:50 +0630

    first commit

---
+++ src/main/java/com/db_test/Main.java
@@ -0,0 +1,19 @@
+package com.db_test;
+
+/**
+    This is the testing Main class
+    Created by :......
+    Created Date:........
+ */
+public class Main {
+    /**
+
+     */
+    public static void main(String[] args) {
+        // This is output.
+        System.out.printf("Hello and welcome!");
+        //.......
+        //for (int i = 1; i <= 5; i++) {
+           //System.out.println("i = " + i);
+        }
+    }
